fa1fa2877d419576665472278853fa90a7ae9452
keitarou <keitarou.oonishi@gmail.com>
2014-04-13 17:58:59 +0900

    oilの設定ファイルの微修正（コメント消しただけ）

--- fuel/app/config/oil.php
+++ fuel/app/config/oil.php
@@ -1,6 +1,6 @@
 <?php
 return array(
 	'phpunit' => array(
-		'binary_path' => 'fuel/vendor/phpunit/phpunit/composer/bin/phpunit',// 個人的には bin/phpunit にエイリアス作って置いてる
+		'binary_path' => 'fuel/vendor/phpunit/phpunit/composer/bin/phpunit'
 	),
 );
