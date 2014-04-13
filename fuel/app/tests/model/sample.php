595139c9b5ee4225adc39a3420feb0f374fccb89
keitarou <keitarou.oonishi@gmail.com>
2014-04-13 13:35:05 +0900

    sample test case

---
+++ fuel/app/tests/model/sample.php
@@ -0,0 +1,21 @@
+<?php
+namespace Fuel\Core;
+
+/**
+ * Sapmle class tests
+ *
+ * @group App
+ */
+class Tests_Sample extends TestCase {
+
+    /**
+     * Tests sample method
+     *
+     * @test
+     */
+    public function test_sample1()
+    {
+        $this->assertEquals(0, 0);
+    }
+
+}
